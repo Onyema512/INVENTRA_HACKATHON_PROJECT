02213092584846f03d3843ac1672f261d64e358b
Onyema512 <aonyema512@gmail.com>
2026-05-31 19:34:39 +0100

    Created landing page hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,12 +1,60 @@
-import React from 'react'
-import '../Css/Hero.css'
+import React from "react";
+import "../Css/Hero.css";
+import {Rocket, Play, CheckCircle,} from "lucide-react";
 
 const Hero = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="hero">
 
-export default Hero
+      <div className="hero-left">
+        <div className="hero-badge">
+          Smart Inventory, Smarter Operations.
+        </div>
+        <h1>
+          Smart Inventory & <br /> Expiry Management <br /> <span>for Modern</span>
+          <span> Supermarkets</span>
+        </h1>
+        <p>
+          Inventra helps supermarkets track inventory in real time,
+          manage sales, reduce expiry losses, and improve staff
+          accountability <br/> all in one powerful platform.
+        </p>
+
+        <div className="hero-buttons">
+          <button className="primary-btn">
+            <Rocket size={18} />
+            Get Started
+          </button>
+          <button className="secondary-btn">
+            <Play size={18} />
+            Book a Demo
+          </button>
+        </div>
+
+        <div className="hero-features">
+           <div className="feature-item">
+              <CheckCircle size={18} />
+              <span>Real-time Tracking</span>
+           </div>
+           <div className="feature-item">
+              <CheckCircle size={18} />
+              <span>Reduce Losses</span>
+           </div>
+           <div className="feature-item">
+              <CheckCircle size={18} />
+              <span>Improve Efficiency</span>
+           </div>
+        </div>
+      </div>
+
+      <div className="hero-right">
+        <div className="dashboard-placeholder">
+          Dashboard Image Here
+        </div>
+      </div>
+
+    </section>
+  );
+};
+
+export default Hero;
